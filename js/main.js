205ecda30f7dbb8f1c7bee549c3c16971a61c095
Miriam Mendoza <miriammp1997@gmail.com>
2017-03-01 13:02:22 -0500

    Probando output

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
+function operacion(){
 var bt = document.getElementById("button");
-bt.onclick() = function(){
+
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
   var op = document.getElementById('operacion').value;
@@ -10,6 +11,7 @@ bt.onclick() = function(){
     case 2: result = num1 - num2; break;
     case 3: result = num1 / num2; break;
     case 4: result = num1* num2; break;
-  }
-  document.getElementById("result").innerHTML = result;
+
+  document.getElementById("result").innerHTML = op;
+}
 }
