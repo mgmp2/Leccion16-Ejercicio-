baf378264914cfe4edd8557c8ec0e4e590db70d9
GreeceArtemis <greece.rapu@gmail.com>
2017-03-01 12:42:07 -0500

    Pregunta siguiente

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function datos(){
+  var nombre=document.getElementById("nombre").value;
+  var num1=document.getElementById("num1").value;
+  var num2=document.getElementById("num2").value;
+  }
+function siguiente()
+  {var txt;
+   var r =confirm ("¿Desea Continuar?");
+   if(r==true){
+     num1=result;
+   }
+   else{
+     alert(result);
+   }
+  }
+
+}
