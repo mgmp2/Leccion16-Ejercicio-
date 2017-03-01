277374febd8111b3799448ecb98ee7aae56465ee
GreeceArtemis <greece.rapu@gmail.com>
2017-03-01 13:36:12 -0500

    html funcionando,juntar todo deben

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ function datos(){
   var num1=document.getElementById("num1").value;
   var num2=document.getElementById("num2").value;
   }
-function siguiente()
+function siguiente(){
+var num1=document.getElementById("num1").value;
+var num2=document.getElementById("num2").value;
   {var txt;
    var r =confirm ("¿Desea Continuar?");
    if(r==true){
      num1=result;
+     document.getElementById("num1").value= num1;
+     alert(num1);
    }
    else{
      alert(result);
