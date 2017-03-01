bfb1c8b489465c7906eb7e09353c161676edec43
Liliana Fiore <liliana.fiorela@gmail.com>
2017-03-01 13:04:11 -0500

    funcion operacion

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function operacion(){
     case "1": result = num1 + num2; break;
     case "2": result = num1 - num2; break;
     case "3": result = num1 / num2; break;
-    case "4": result = num1* num2; break;
+    case "4": result = num1 * num2; break;
   }
   document.getElementById("result").innerHTML = result;
 }
